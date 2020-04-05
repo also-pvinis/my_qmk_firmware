2d4cf3fa1b61b24d016c05ce101a5d6ea7992c76
Pavlos Vinieratos <pvinis@gmail.com>
2020-04-05 12:22:12 +0200

    merge?

--- pvinis/copy-from-og.sh
+++ pvinis/copy-from-og.sh
@@ -23,9 +23,9 @@ rsync -r $BASE/ $MINE  \
 # ergodox_ez
 rsync -r $BASE/keyboards/ergodox_ez/ $MINE/keyboards/ergodox_ez \
  --exclude='keymaps'
-rsync -r $BASE/keyboards/ergodox_ez/keymaps/pvinis/ $MINE/keyboards/ergodox_ez/keymaps/pvinis
+# rsync -r $BASE/keyboards/ergodox_ez/keymaps/pvinis/ $MINE/keyboards/ergodox_ez/keymaps/pvinis
 
 # keebio/iris
 rsync -r $BASE/keyboards/keebio/iris/ $MINE/keyboards/keebio/iris \
  --exclude='keymaps'
-rsync -r $BASE/keyboards/keebio/iris/keymaps/pvinis/ $MINE/keyboards/keebio/iris/keymaps/pvinis
+# rsync -r $BASE/keyboards/keebio/iris/keymaps/pvinis/ $MINE/keyboards/keebio/iris/keymaps/pvinis
