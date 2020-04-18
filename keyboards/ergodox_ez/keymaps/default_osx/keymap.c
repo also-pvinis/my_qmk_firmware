7bf87e6aa0c428608886b589a10a82b45a3ce483
Pavlos Vinieratos <pvinis@gmail.com>
2020-04-18 10:37:18 +0200

    rework some keys

--- keyboards/ergodox_ez/keymaps/default_osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/default_osx/keymap.c
@@ -5,53 +5,51 @@
 #include "debug.h"
 #include "action_layer.h"
 
-#define BASE 0 // default layer
+#define BASE 0 // Call of Duty
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
+/* Keymap 0: Call of Duty
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|  LGui  |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ * ,--------------------------------------------------.
+ * |   esc  |   1  |   2  |   3  |   4  |   5  |     |
+ * |--------+------+------+------+------+-------------|
+ * |   tab  |   Q  |   W  |   E  |   R  |   T  |      |
+ * |--------+------+------+------+------+------|      |
+ * |  ctrl  |   A  |   S  |   D  |   F  |   G  |------|
+ * |--------+------+------+------+------+------|      |
+ * |  shift |   Z  |   X  |   C  |   V  |   B  |      |
+ * `--------+------+------+------+------+-------------'
+ *   |      |      |      |      |  spc |
+ *   `----------------------------------'
+ *                                        ,-------------.
+ *                                        |      |      |
+ *                                 ,------|------|------|
+ *                                 |      |      |      |
+ *                                 |  alt |  M   |------|
+ *                                 |      |      |      |
+ *                                 `--------------------'
  */
-// If it accepts an argument (i.e, is a function), it doesn't need KC_.
-// Otherwise, it needs KC_*
-[BASE] = LAYOUT_ergodox(  // layer 0 : default
+[BASE] = LAYOUT_ergodox(
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DEL,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,  KC_M ,
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_TAB,
+        KC_ESC ,  KC_1   ,  KC_2   ,  KC_3   ,  KC_4   ,   KC_5,  KC_TRNS,
+        KC_TAB ,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,   KC_T,  KC_TRNS,
+        KC_LCTL,  KC_A   ,  KC_S   ,  KC_D   ,  KC_F   ,   KC_G,
+        KC_LSFT,  KC_Z   ,  KC_X   ,  KC_C   ,  KC_V   ,   KC_B,  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_SPC ,
+                                                        KC_TRNS,  KC_TRNS,
+                                                                  KC_TRNS,
+                                               KC_LALT,  KC_M  ,  KC_TRNS,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_LGUI,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          TT(SYMB),
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+             KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+             KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+                       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+             KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+             KC_TRNS,  KC_TRNS,
+             KC_TRNS,
+             KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
 /* Keymap 1: Symbol Layer)()``````g`gg``ggg
  *
